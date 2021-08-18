d8bbd4029a55a7c16a9ed5018d3987737481cbc2
dipalimagare <kalpeshmahajan90@gmail.com>
2021-08-18 15:39:06 +0530

    Uc7

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,45 +1,62 @@
 package com.bl;
 
 import java.util.Random;
-
 public class EmployeeWage {
+	private static final int IS_FUllTime = 1;
+	private static final int IS_PARTTIME = 2;
+	private static final int IS_ABSENT = 0;
+	private String companyName;
+	private int max_working_hrs;
+	private int days_in_month;
+	private int wage_per_hr;
+
+	EmployeeWage(String companyName, int max_working_hrs, int days_in_month, int wage_per_hr) {
+		this.companyName = companyName;
+		this.max_working_hrs = max_working_hrs;
+		this.days_in_month = days_in_month;
+		this.wage_per_hr = wage_per_hr;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation program");
-		calculatingWage();
+		// TODO Auto-generated method stub
+
+		EmployeeWage e1 = new EmployeeWage("d-mart", 100, 20, 100);
+		EmployeeWage e2 = new EmployeeWage("jio-Mart", 150, 25, 110);
+		e1.employeeWage();
+		e2.employeeWage();
 	}
-	
-	public static void calculatingWage() {
-	int empHrs = 0;
-	int totalWorkingDays = 0;
-	int workingHr = 0;
-	int empWage = 0;
-	int wagePerHr = 20;
-	int maxHrs = 100;
-	int noOfWorkingDays = 100;
 
-	while (empHrs <= maxHrs && totalWorkingDays <= noOfWorkingDays) {
-		Random r = new Random();
-		int empCheck = r.nextInt(3);
-		switch (empCheck) {
-		case 0: {
-
-			workingHr = 8;
-			break;
+	// Checking the employee attendance and paying salary based on their working
+	// hours
+	void employeeWage() {
+		int working_hr = 0;
+		int total_working_hr = 0;
+		int monthly_total_wage = 0;
+		int daily_wage_array[] = new int[days_in_month];
+		int days = 0;
+		while (days < days_in_month && total_working_hr < max_working_hrs) {
+			int emp_check = (int) (Math.floor(Math.random() * 10)) % 3;
+			switch (emp_check) {
+			case IS_FUllTime -> {
+				working_hr = 8;
+			}
+			case IS_PARTTIME -> {
+				working_hr = 4;
+			}
+			case IS_ABSENT -> {
+				working_hr = 0;
+			}
+			}
+			total_working_hr += working_hr;
+			daily_wage_array[days] = working_hr * wage_per_hr;
+			days++;
 		}
-		case 1: {
-			workingHr = 4;
-			break;
+		for (int j = 0; j < days_in_month; j++) {
+			int day = j + 1;
+			System.out.println("Day " + day + " wage is " + daily_wage_array[j]);
+			monthly_total_wage = monthly_total_wage + daily_wage_array[j];
 		}
-		case 2: {
-			workingHr = 0;
-			break;
-		}
-		}
-		empHrs += workingHr;
-		empWage = empWage + (workingHr * wagePerHr);
+		System.out.println("Monthly wage for " + companyName + " : " + monthly_total_wage + "\n");
 	}
-	System.out.println("Employee Wage is : " + empWage);
-
-}
 
 }

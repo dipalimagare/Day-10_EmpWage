3a6e93f98416dc3c005132909c41a5345d37e903
dipalimagare <kalpeshmahajan90@gmail.com>
2021-08-18 13:36:30 +0530

    Uploading Uc1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,25 @@
 package com.bl;
 
-
 import java.util.Random;
 
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation program");
+		checkAttendance();
 	}
 
+	public static void checkAttendance() {
+		Random r = new Random();
+		int empCheck = r.nextInt(2);
+		switch (empCheck) {
+		case 0: {
+			System.out.println("Employee is present");
+			break;
+		}
+		case 1: {
+			System.out.println("Employee is absent");
+			break;
+		}
+		}
+	}
 }
-

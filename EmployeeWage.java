8d088361a80f839b7673e106cb07affe03a1adca
dipalimagare <kalpeshmahajan90@gmail.com>
2021-08-18 15:10:42 +0530

    Uc6

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,37 +7,39 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Computation program");
 		calculatingWage();
 	}
+	
 	public static void calculatingWage() {
-		int empHrs = 0;
-		int totalWorkingDays = 0;
-		int workingHr = 0;
-		int empWage = 0;
-		int wagePerHr = 20;
-		int maxHrs = 100;
-		int noOfWorkingDays = 100;
+	int empHrs = 0;
+	int totalWorkingDays = 0;
+	int workingHr = 0;
+	int empWage = 0;
+	int wagePerHr = 20;
+	int maxHrs = 100;
+	int noOfWorkingDays = 100;
 
-		while (empHrs <= maxHrs && totalWorkingDays <= noOfWorkingDays) {
-			Random r = new Random();
-			int empCheck = r.nextInt(3);
-			switch (empCheck) {
-			case 0: {
+	while (empHrs <= maxHrs && totalWorkingDays <= noOfWorkingDays) {
+		Random r = new Random();
+		int empCheck = r.nextInt(3);
+		switch (empCheck) {
+		case 0: {
 
-				workingHr = 8;
-				break;
-			}
-			case 1: {
-				workingHr = 4;
-				break;
-			}
-			case 2: {
-				workingHr = 0;
-				break;
-			}
-			}
-			empHrs += workingHr;
-			empWage = empWage + (workingHr * wagePerHr);
+			workingHr = 8;
+			break;
 		}
-		System.out.println("Employee wage is : " + empWage);
-
+		case 1: {
+			workingHr = 4;
+			break;
+		}
+		case 2: {
+			workingHr = 0;
+			break;
+		}
+		}
+		empHrs += workingHr;
+		empWage = empWage + (workingHr * wagePerHr);
 	}
+	System.out.println("Employee Wage is : " + empWage);
+
+}
+
 }
